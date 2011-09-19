7d60ca8d5d407e9ab94f1e4a8f30003408a4a3cf
Martijn Dashorst <martijn.dashorst@gmail.com>
2011-09-19 23:45:39 +0200

    Heroku tutorial 5 Start class

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,48 +1,25 @@
 
-
-import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Start {
 
 	public static void main(String[] args) throws Exception {
-        Server server = new Server();
-        SocketConnector connector = new SocketConnector();
+		String webPort = System.getenv("PORT");
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "8080";
+		}
+		String webappDirLocation = "src/main/webapp/";
 
-        // Set some timeout options to make debugging easier.
-        connector.setMaxIdleTime(1000 * 60 * 60);
-        connector.setSoLingerTime(-1);
+		Server server = new Server(Integer.valueOf(webPort));
+		WebAppContext root = new WebAppContext();
+		root.setContextPath("/");
+		root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+		root.setResourceBase(webappDirLocation);
+		root.setParentLoaderPriority(true);
 
-        String webPort = System.getenv("PORT");
-    	if(webPort == null || webPort.isEmpty()) {
-        	webPort = "8080";
-    	}
-        connector.setPort(Integer.valueOf(webPort));
-        server.setConnectors(new Connector[] { connector });
-
-        WebAppContext bb = new WebAppContext();
-        bb.setServer(server);
-        bb.setContextPath("/");
-        bb.setWar("src/main/webapp");
-        bb.setParentLoaderPriority(true);
-
-        // START JMX SERVER
-        // MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-        // MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
-        // server.getContainer().addEventListener(mBeanContainer);
-        // mBeanContainer.start();
-
-        server.setHandler(bb);
-
-        try {
-            System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
-            server.start();
-            server.join();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(100);
-        }
+		server.setHandler(root);
+		server.start();
+		server.join();
 	}
 }
